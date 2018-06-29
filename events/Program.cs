eb8e7a5a23478af4273fe5a14881e546ff5b70e8
Carlo <carlograna@hotmail.com>
2018-06-29 11:37:46 -0500

    simple event example

--- events/Program.cs
+++ events/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace events
+namespace Play
 {
-    class Program
+    class TrainSignal
     {
-        static void Main(string[] args)
+        public delegate void DelRunThis();
+        public event DelRunThis del_run_this;
+
+        public void TrainIsComing()
         {
+            // stop the car or whatever object wants to cross
+            if (del_run_this != null)
+            {
+                del_run_this();
+            }
         }
     }
+
+    class Car
+    {
+        public Car(TrainSignal trainSignal)
+        {
+            trainSignal.del_run_this += StopTheCar;
+        }
+
+
+        void StopTheCar()
+        {
+            Console.WriteLine("Screeeeeeetch");
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            TrainSignal trainSignal = new TrainSignal();
+
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+            new Car(trainSignal);
+
+            trainSignal.del_run_this += null;
+
+            trainSignal.TrainIsComing();
+            Console.Read();
+        }
+    }
+
+
 }
